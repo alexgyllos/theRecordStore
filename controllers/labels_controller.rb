5af92bae4dd4304782e3daef55c002b0078a4435
Alex Gyllos <alex.gyllos@gmail.com>
2020-04-04 11:20:02 +0100

    adds edit and delete function to label

--- controllers/labels_controller.rb
+++ controllers/labels_controller.rb
@@ -3,12 +3,41 @@ require( 'sinatra/contrib/all' )
 require_relative( '../models/label.rb' )
 also_reload( '../models/*' )
 
+# INDEX
+
 get '/labels' do
   @labels = Label.all()
   erb (:"labels/index")
 end
 
+# EDIT
+
+get '/labels/:id/edit' do
+  id = params['id'].to_i
+  @label = Label.find(id)
+  erb(:"labels/edit")
+end
+
+# SHOW
+
 get '/labels/:id' do
   @label = Label.find(params['id'].to_i)
   erb(:"labels/show")
+end
+
+# DELETE
+
+post '/labels/:id/delete' do
+  id = params['id'].to_i
+  label = Label.find(id)
+  label.delete()
+  redirect('/')
+end
+
+# POST EDIT
+
+post '/labels/:id' do
+  updated_label = Label.new(params)
+  updated_label.update()
+  redirect('/labels/' + params['id'])
 end
