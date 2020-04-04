a8f12a168e0306a60355c0c56690e77fc51b0a89
Alex Gyllos <alex.gyllos@gmail.com>
2020-04-04 10:51:57 +0100

    adds edit and delete function to artist

--- controllers/artists_controller.rb
+++ controllers/artists_controller.rb
@@ -3,12 +3,37 @@ require('sinatra/contrib/all')
 require_relative('../models/artist.rb')
 also_reload('../models/*')
 
+# INDEX
+
 get '/artists' do
   @artists = Artist.all()
   erb( :"artists/index" )
 end
 
+# EDIT
+
+get '/artists/:id/edit' do
+  id = params['id'].to_i
+  @artist = Artist.find(id)
+  erb(:"artists/edit")
+end
+
+# SHOW
+
 get '/artists/:id' do
   @artist = Artist.find(params['id'].to_i)
   erb( :"artists/show")
+end
+
+post '/artists/:id/delete' do
+  id = params['id'].to_i
+  artist = Artist.find(id)
+  artist.delete()
+  redirect('/')
+end
+
+post '/artists/:id' do
+  updated_artist = Artist.new(params)
+  updated_artist.update()
+  redirect('/artists/' + params['id'])
 end
